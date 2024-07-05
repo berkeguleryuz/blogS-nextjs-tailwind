bc307df38ba2a986da8052fe94fd8932af1fbc89
Berke Güleryüz <berkeguleryuz@gmail.com>
2024-07-05 11:25:28 +0200

    update

---
+++ constants/site.ts
@@ -0,0 +1,19 @@
+export const siteMetadata = {
+  title: "Next.js Blog with Optimized Seo ",
+  author: "Clodron",
+  headerTitle: "Next.js Blog",
+  description: "A blog created with Next.js, Tailwind, and TypeScript.",
+  language: "en-us",
+  theme: "dark",
+  siteUrl: "https://www.clodron.com", // your website URL
+  siteLogo: "/logo.png",
+  socialBanner: "/mockprofile.png", // add social banner in the public folder
+  email: "berke@clodron.com",
+  github: "https://github.com/berkeguleryuz",
+  twitter: "https://twitter.com/omegayon",
+  facebook: "https://facebook.com/berkeguleryuz1",
+  youtube: "https://youtube.com/@omegayon",
+  linkedin: "https://linkedin.com/in/berke-guleryuz/",
+  dribbble: "https://www.dribbble.com",
+  locale: "en-US",
+};
